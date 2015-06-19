5765a3a7a2933cbb74841aa94dabf2e9699fd64c
mihyeKim <modong518@gmail.com>
2015-06-19 16:17:26 +0900

    create gridLayout

--- MyLibTest/gen/com/test/mihye/R.java
+++ MyLibTest/gen/com/test/mihye/R.java
@@ -39,7 +39,7 @@ public final class R {
         public static final int p_bg_basic2=0x7f020008;
     }
     public static final class id {
-        public static final int View_layout=0x7f06000c;
+        public static final int View_layout=0x7f06000d;
         public static final int action_flip=0x7f060001;
         /** 
         These action bar item IDs (menu item IDs) are defined here for
@@ -50,22 +50,23 @@ public final class R {
     
          */
         public static final int action_next=0x7f060000;
-        public static final int action_settings=0x7f060013;
-        public static final int button=0x7f060005;
+        public static final int action_settings=0x7f060014;
+        public static final int button=0x7f060006;
         public static final int button1=0x7f060003;
-        public static final int listView=0x7f060004;
-        public static final int listview_line_Textview=0x7f060006;
-        public static final int main_tab1=0x7f060007;
-        public static final int main_tab2=0x7f060008;
-        public static final int main_tab3=0x7f060009;
-        public static final int main_tab4=0x7f06000a;
-        public static final int main_tab5=0x7f06000b;
-        public static final int popup_bg=0x7f06000d;
-        public static final int popup_view_button=0x7f060010;
-        public static final int popup_view_content=0x7f06000f;
-        public static final int popup_view_title=0x7f06000e;
-        public static final int tabview_layout_textView1=0x7f060011;
-        public static final int tabview_main_viewpager=0x7f060012;
+        public static final int gridlayout=0x7f060004;
+        public static final int listView=0x7f060005;
+        public static final int listview_line_Textview=0x7f060007;
+        public static final int main_tab1=0x7f060008;
+        public static final int main_tab2=0x7f060009;
+        public static final int main_tab3=0x7f06000a;
+        public static final int main_tab4=0x7f06000b;
+        public static final int main_tab5=0x7f06000c;
+        public static final int popup_bg=0x7f06000e;
+        public static final int popup_view_button=0x7f060011;
+        public static final int popup_view_content=0x7f060010;
+        public static final int popup_view_title=0x7f06000f;
+        public static final int tabview_layout_textView1=0x7f060012;
+        public static final int tabview_main_viewpager=0x7f060013;
         public static final int textView1=0x7f060002;
     }
     public static final class integer {
@@ -77,13 +78,14 @@ public final class R {
         public static final int activity_tab1=0x7f030001;
         public static final int activity_tab2=0x7f030002;
         public static final int activity_tab3=0x7f030003;
-        public static final int listview_layout=0x7f030004;
-        public static final int listview_line=0x7f030005;
-        public static final int main_tab_layout=0x7f030006;
-        public static final int popup_layout=0x7f030007;
-        public static final int tabview_layout=0x7f030008;
-        public static final int tabview_main_layout=0x7f030009;
-        public static final int test_layout=0x7f03000a;
+        public static final int grid_layout=0x7f030004;
+        public static final int listview_layout=0x7f030005;
+        public static final int listview_line=0x7f030006;
+        public static final int main_tab_layout=0x7f030007;
+        public static final int popup_layout=0x7f030008;
+        public static final int tabview_layout=0x7f030009;
+        public static final int tabview_main_layout=0x7f03000a;
+        public static final int test_layout=0x7f03000b;
     }
     public static final class menu {
         public static final int popup_test=0x7f090000;
