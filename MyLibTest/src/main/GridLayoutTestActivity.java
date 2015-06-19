5feffba1404635394befc7934b02189797c253c3
mihyeKim <modong518@gmail.com>
2015-06-19 16:18:02 +0900

    create grid layout

---
+++ MyLibTest/src/main/GridLayoutTestActivity.java
@@ -0,0 +1,19 @@
+package main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.test.mihye.R;
+
+/**
+ * Created by cosquare on 2015-06-19.
+ */
+public class GridLayoutTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.grid_layout);
+    }
+}
