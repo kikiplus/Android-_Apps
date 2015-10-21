d03c7ef31d6e204e85e326f0322cd1862523b7df
grapegirl <modong518@gmail.com>
2015-10-22 00:51:39 +0900

    다음 지도 소스 수정

--- MyLibTest/src/com/kiki/View/Activity/TestB/DaumMapActivity.java
+++ MyLibTest/src/com/kiki/View/Activity/TestB/DaumMapActivity.java
@@ -1,13 +1,16 @@
 package com.kiki.View.Activity.TestB;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.kiki.View.R;
 import com.kiki.android.Utils.ContextUtils;
@@ -30,6 +33,12 @@ public class DaumMapActivity extends Activity implements View.OnClickListener {
 
     private Location mLocation;
 
+    private ToggleButton mToggleButton;
+
+    private final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+
+    private final long MIN_TIMEE_CHANGE_FOR_UPDATES = 60 * 1000 * 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,44 +53,80 @@ public class DaumMapActivity extends Activity implements View.OnClickListener {
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
-        findViewById(R.id.button).setOnClickListener(this);
+        mToggleButton =  (ToggleButton)findViewById(R.id.map_button);
+        mToggleButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.button:
-                LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+            case R.id.map_button:
+                if(mToggleButton.isChecked()){
+                    getLocation();
+                }else{
+                    stopLocation();
+                }
                 break;
         }
     }
 
-    LocationListener mLocationListener = new LocationListener() {
+    /**
+     * 내 위치 가져오기
+     */
+    private void getLocation(){
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        boolean isGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if(isGps){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIMEE_CHANGE_FOR_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, mLocationListener);
+            if(locationManager != null){
+                mLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+        }else{
+            boolean isNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIMEE_CHANGE_FOR_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, mLocationListener);
+            if(locationManager != null){
+                mLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+        }
+        mapView.setMapCenterPoint(MapPoint.mapPointWithCONGCoord(mLocation.getLatitude(), mLocation.getLongitude()), true);
+        setMarker(MapPoint.mapPointWithCONGCoord(mLocation.getLatitude(), mLocation.getLongitude()));
+
+    }
+
+    /**
+     * 내 위치 가져오기 중지 메소드
+     */
+    private void stopLocation(){
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationManager.removeUpdates(mLocationListener);
+    }
+    /**
+     * 내위치 정보 리스너
+     */
+    private LocationListener mLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
-            // Called when a new location is found by the network location provider.
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
-
-            Toast.makeText(getApplicationContext(), latitude + ", " + longitude, Toast.LENGTH_LONG).show();
-            mapView.setMapCenterPoint(MapPoint.mapPointWithCONGCoord(latitude, longitude), true);
-            setMarker(MapPoint.mapPointWithCONGCoord(latitude, longitude));
+            Toast.makeText(getApplicationContext(), "위치정보 값 변경 : " +  latitude + ", " + longitude, Toast.LENGTH_LONG).show();
         }
 
         public void onStatusChanged(String provider, int status, Bundle extras) {
+            Toast.makeText(getApplicationContext(), "GPS 상태 변경 : " +  status, Toast.LENGTH_LONG).show();
         }
 
         public void onProviderEnabled(String provider) {
+            Toast.makeText(getApplicationContext(), "GPS 상태 가능 : " +  provider, Toast.LENGTH_LONG).show();
         }
 
         public void onProviderDisabled(String provider) {
+            Toast.makeText(getApplicationContext(), "GPS 상태 불가능 : " +  provider, Toast.LENGTH_LONG).show();
         }
     };
 
     /**
      * 마커 추가
      */
-    public void setMarker(MapPoint mapPoint){
+    private void setMarker(MapPoint mapPoint){
         MapPOIItem marker = new MapPOIItem();
         marker.setItemName("Default Marker");
         marker.setTag(0);
